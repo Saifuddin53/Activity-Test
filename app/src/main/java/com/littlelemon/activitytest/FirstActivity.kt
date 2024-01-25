bdbe23a1dc2dea4e64caf804b6aa5d1a0096d4d2
Saifuddin <adenwala32@gmail.com>
2024-01-25 10:37:47 +0530

    top bar from material design added

--- app/src/main/java/com/littlelemon/activitytest/FirstActivity.kt
+++ app/src/main/java/com/littlelemon/activitytest/FirstActivity.kt
@@ -1,36 +1,32 @@
 package com.littlelemon.activitytest
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun FirstActivity() {
 
@@ -41,7 +37,9 @@ fun FirstActivity() {
         modifier = Modifier
             .fillMaxSize()
     ){
-        TopAppBar(count, onValueChange = {count = count + 1})
+//        TopAppBar(count, onValueChange = {count = count + 1})
+        
+        CenterAlignedTopAppBarExample()
         Row(
             horizontalArrangement = Arrangement.Center,
             verticalAlignment = Alignment.CenterVertically,
@@ -61,33 +59,82 @@ fun FirstActivity() {
     }
 }
 
+//@Composable
+//fun TopAppBar(
+//    count: Int,
+//    onValueChange: () -> Unit
+//) {
+//    Row(
+//        horizontalArrangement = Arrangement.SpaceBetween,
+//        verticalAlignment = Alignment.CenterVertically,
+//        modifier = Modifier
+//            .fillMaxWidth()
+//            .background(color = Color(0xFF00FF00))
+//    ) {
+//        IconButton(onClick = onValueChange) {
+//            Icon(
+//                painter = painterResource(id = R.drawable.baseline_arrow_drop_down_24),
+//                contentDescription = "Navigation drawer",
+//            )
+//        }
+//        Text(
+//            text = stringResource(id = R.string.app_name) + " " + count,
+//            fontSize = 24.sp
+//        )
+//        IconButton(onClick = onValueChange) {
+//            Icon(
+//                painter = painterResource(id = R.drawable.share_24px),
+//                contentDescription = "Share"
+//            )
+//        }
+//    }
+//}
+
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TopAppBar(
-    count: Int,
-    onValueChange: () -> Unit
-) {
-    Row(
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(color = Color(0xFF00FF00))
+fun CenterAlignedTopAppBarExample() {
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+
+    Scaffold(
+        modifier = Modifier,
+
+        topBar = {
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text(
+                        "Centered Top App Bar",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Menu,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+                scrollBehavior = scrollBehavior,
+            )
+        },
     ) {
-        IconButton(onClick = onValueChange) {
-            Icon(
-                painter = painterResource(id = R.drawable.baseline_arrow_drop_down_24),
-                contentDescription = "Navigation drawer",
-            )
-        }
-        Text(
-            text = stringResource(id = R.string.app_name) + " " + count,
-            fontSize = 24.sp
-        )
-        IconButton(onClick = onValueChange) {
-            Icon(
-                painter = painterResource(id = R.drawable.share_24px),
-                contentDescription = "Share"
-            )
-        }
     }
 }
+
+
